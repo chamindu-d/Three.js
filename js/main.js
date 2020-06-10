6b1158866451907b6819b64e881c410bf353a1c0
Chamindu Dayajeewa <cdayajeewa@gmail.com>
2020-06-10 19:17:01 +1000

    Fixed orbital physics

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@ function handleEnvironment() {
 
   const environment = new Environment('white');
 
-  const light = new THREE.DirectionalLight('white', 1.5);
-  light.position.set(0, 0, -100);
-  environment.scene.add(light);
-
-  const earth = new PhysicsBody(new THREE.Mesh(new THREE.SphereGeometry(1, 40, 40), new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('resources/img/earth_texture.jpg')})), 10);
-  earth.mesh.position.set(0, 0, 0);
-  environment.scene.add(earth.mesh);
+  const controls = new THREE.OrbitControls(environment.camera, environment.renderer.domElement);
+  controls.target = new THREE.Vector3(0, 0, 0);
+  controls.minDistance = 15;
+  controls.maxDistance = 700;
+  controls.enablePan = false;
+  environment.camera.position.y = 200;
+  controls.update();
 
   const backgroundTexture = new THREE.CubeTextureLoader().load([
       'resources/img/background_map_horizontal.jpg',
@@ -42,18 +42,22 @@ function handleEnvironment() {
   backgroundTexture.minFilter = THREE.LinearFilter;
   environment.scene.background = backgroundTexture;
 
-  // const controls = new THREE.OrbitControls(environment.camera, environment.renderer.domElement);
-  // controls.enablePan = false;
-  // environment.camera.position.z = 5;
-  // controls.update();
+  const light = new THREE.PointLight('white', 2, 0, 2);
+  light.position.set(0, 0, 500);
+  environment.scene.add(light);
 
-  var cameraAngles = {x: 0, y: 0};
-  var zoom = 5;
+  const earth = new PhysicsBody(6.371, 5.972 * Math.pow(10, 7), new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('resources/img/earth_texture.jpg')}));
+  earth.mesh.position.set(0, 0, 0);
+  environment.scene.add(earth.mesh);
+
+  const moon = new PhysicsBody(1.737, 7.348 * Math.pow(10, 5), new THREE.MeshLambertMaterial({color: 'lightgray'}));
+  moon.mesh.position.set(0, 0, 384.4);
+  moon.velocity = new THREE.Vector3(2, 0, 0);
+  environment.scene.add(moon.mesh);
 
   environment.handleRendering(function () {
 
-    // Rotate camera around earth
-    cameraAngles = rotateAroundObject(environment.camera, earth.mesh, zoom, 0.02, cameraAngles, keys[37], keys[39], keys[38], keys[40]);
+    moon.stagePhysicsAround(earth);
 
     // Rotate earth around its axis
     earth.mesh.rotation.y += 0.0025;
--- js/modules.js
+++ js/modules.js
@@ -26,14 +26,24 @@ class Environment {
 // Class for a property orientated physical body
 class PhysicsBody {
 
-  constructor(mesh, mass) {
+  constructor(radius, mass, material) {
 
-    this.mesh = mesh;
+    this.radius = radius;
     this.mass = mass;
-    this.radius = this.mesh.geometry.parameters.radius;
-    this.velocity = new THREE.Vector3(2, 3, 4);
-    this.momentum = this.velocity.multiplyScalar(this.mass);
-    this.gravityAcceleration = (6.67430 * Math.pow(10, -11)) * this.mass / Math.pow(this.radius, 2);
+    this.mesh = new THREE.Mesh(new THREE.SphereGeometry(radius, 100, 100), material);
+    this.velocity = new THREE.Vector3(0, 0, 0);
+    this.acceleration = new THREE.Vector3(0, 0, 0);
+    this.gAcceleration = (6.67430 * Math.pow(10, -11)) * this.mass;
+
+  }
+
+  stagePhysicsAround(object) {
+
+    const gravity = object.gAcceleration * (this.mass / Math.pow(distanceBetween(this, object), 2));
+
+    this.acceleration = directionalVector(this, object, gravity);
+    this.velocity.add(this.acceleration);
+    this.mesh.position.add(this.velocity);
 
   }
 
@@ -54,12 +64,35 @@ function rotateAroundObject(camera, object, radius, scale, anglePair, xPositive,
 
 }
 
+// Find a vector directed towards another PhysicsBody object given a magnitude
+function directionalVector(source, destination, magnitude) {
+
+  const xDistance = source.mesh.position.x - destination.mesh.position.x;
+  const yDistance = source.mesh.position.y - destination.mesh.position.y;
+  const zDistance = source.mesh.position.z - destination.mesh.position.z;
+  const horizontalDistance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(zDistance, 2));
+
+  const theta = Math.asin(xDistance / horizontalDistance) + (Math.PI / 2);
+  const horizontalFactor = (zDistance != 0) ? -(zDistance / Math.abs(zDistance)) : 0;
+  const verticalFactor = (yDistance != 0) ? -(yDistance / Math.abs(yDistance)) : 0;
+
+  const directionalVector = new THREE.Vector3(magnitude * Math.cos(theta), verticalFactor * magnitude, horizontalFactor * magnitude * Math.sin(theta));
+
+  return directionalVector;
+
+}
+
+// Find the distance between two PhysicsBody objects
+function distanceBetween(a, b) {
+  return a.mesh.position.distanceTo(b.mesh.position);
+}
+
 // Check if WebGL is supported on the client
 function graphicsAvailable() {
   return (document.createElement('canvas').getContext('webgl') !=  null);
 }
 
-// Convert radians to degrees
-function degrees(radians) {
-  return radians * 180 / Math.PI;
+// (testing purposes only)
+function displayVector(vector, isAlert) {
+  (isAlert == true) ? alert(vector.x + "," + vector.y + "," + vector.z) : console.log(vector.x + "," + vector.y + "," + vector.z);
 }
